763a2a35790750ce74722d6c4fa4df8bf8015810
Ramkrishn <ram706860@gmail.com>
2023-08-22 23:59:24 +0530

    fix the scroll for small width devices

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -3,13 +3,13 @@ import skills from "../../utils/skills";
 const Skill = () => {
   console.log(skills.length);
   return (
-    <div className="skill_container w-full overflow-hidden my-12 py-12">
+    <div className="skill_container w-full overflow-hidden mt-8 py-12">
       <div className="skill_slide grid grid-flow-col">
         {skills.map((skill) => {
           const { language, experience, icon } = skill;
           return (
             <div
-              className="skill flex flex-col items-center justify-center gap-4 sm:gap-6 text-center w-52 px-4"
+              className="skill flex flex-col items-center justify-center gap-4 sm:gap-6 text-center w-44 sm:w-52 px-4"
               key={Date.now() * Math.random()}
             >
               <div className="w-20 h-20 object-contain">{icon}</div>
